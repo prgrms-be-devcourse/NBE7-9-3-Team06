13d65f70ef4a80d290e76ba9d424a8358145053b
Hodakrer <jihyuk0609@naver.com>
2025-11-14 13:59:21 +0900

    [Migration] 스케줄러 도메인을 코틀린으로 마이그레이션 (#4)
    * Rename .java to .kt
    
    * migration : scheduler 패키지를 kotlin으로 마이그레이션
    - 1. 안 쓰는 ExecutionTimer 관련 코드 삭제
    - 2. memoryMonitor 관련 코드 코틀린으로 변경
    - 3. 실행 코드인 OrderUpdateStatusScheduler.kt를 코틀린으로 변경
    
    * migration : scheduler 패키지를 kotlin으로 마이그레이션
    - 롬복 어노테이션인 Slf4j 삭제

---
+++ backend/src/main/java/com/backend/petplace/global/scheduler/managementfactory/MemoryMonitor.kt
@@ -0,0 +1,9 @@
+package com.backend.petplace.global.scheduler.managementfactory
+
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MemoryMonitor 
