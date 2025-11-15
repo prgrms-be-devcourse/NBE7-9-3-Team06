348dad1041df9a902792af466c66ccde0b83d922
dpwl0974 <dpwl0974@gmail.com>
2025-11-15 14:25:51 +0900

    refactor: JPQL 쿼리 멀티라인 변경

--- backend/src/main/java/com/backend/petplace/domain/review/repository/ReviewRepository.kt
+++ backend/src/main/java/com/backend/petplace/domain/review/repository/ReviewRepository.kt
@@ -14,24 +14,26 @@ interface ReviewRepository : JpaRepository<Review, Long> {
 
     fun findByPlaceOrderByIdDesc(place: Place): List<Review>
 
-    @Query(
-        ("SELECT new com.backend.petplace.domain.review.dto.response.MyReviewResponse(" +
-                "r.id, r.place.id, r.place.name, r.place.address, " +
-                "r.rating, r.content, r.imageUrl, r.createdDate, pt.amount) " +
-                "FROM Review r " +
-                "JOIN r.place pl " +
-                "JOIN r.user u " +
-                "LEFT JOIN r.point pt " +
-                "WHERE r.user = :user")
-    )
+    @Query("""
+        SELECT new com.backend.petplace.domain.review.dto.response.MyReviewResponse(
+                r.id, r.place.id, r.place.name, r.place.address,
+                r.rating, r.content, r.imageUrl, r.createdDate, pt.amount
+        ) 
+        FROM Review r
+        JOIN r.place pl
+        JOIN r.user u
+        LEFT JOIN r.point pt
+        WHERE r.user = :user
+    """)
     fun findMyReviewsWithProjection(@Param("user") user: User): List<MyReviewResponse>
 
-    @Query(
-        ("SELECT new com.backend.petplace.domain.review.dto.ReviewInfo(" +
-                "r.id, u.nickName, r.content, r.rating, r.imageUrl, r.createdDate) " +
-                "FROM Review r " +
-                "JOIN r.user u " +
-                "WHERE r.place = :place")
-    )
+    @Query("""
+        SELECT new com.backend.petplace.domain.review.dto.ReviewInfo(
+                r.id, u.nickName, r.content, r.rating, r.imageUrl, r.createdDate
+        )
+        FROM Review r 
+        JOIN r.user u 
+        WHERE r.place = :place
+    """)
     fun findReviewInfosByPlaceWithProjection(@Param("place") place: Place): List<ReviewInfo>
 }
