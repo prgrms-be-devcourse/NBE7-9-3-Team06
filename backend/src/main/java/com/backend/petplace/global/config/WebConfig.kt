8b89f15519703c28313ae2f7bf61cb221c5ce84e
dpwl0974 <dpwl0974@gmail.com>
2025-11-15 15:46:17 +0900

    migration: webconfig 변환

---
+++ backend/src/main/java/com/backend/petplace/global/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package com.backend.petplace.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
+
