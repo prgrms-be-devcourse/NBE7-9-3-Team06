9c1eed4d11cc34a9a05005f7c080390bfb149b47
Ho-JunYu <hojoon1011@gmail.com>
2025-11-16 02:36:52 +0900

    refactor: KcisaImportService.upsert()에서 신규 Place 생성 분기 Kotlin 방식으로 리팩토링

--- backend/src/main/java/com/backend/petplace/domain/place/importer/KcisaImportService.java
+++ backend/src/main/java/com/backend/petplace/domain/place/importer/KcisaImportService.java
@@ -61,24 +61,27 @@ public class KcisaImportService {
           f.tel(), f.url(), f.postalCode(), f.address(), f.latitude(), f.longitude(), f.rawDescription()
       );
     } else {
-      Place e = Place.builder()
-          .uniqueKey(f.uniqueKey())
-          .name(f.name())
-          .category1(f.category1())
-          .category2(f.category2())
-          .openingHours(f.openingHours())
-          .closedDays(f.closedDays())
-          .parking(f.parking())
-          .petAllowed(f.petAllowed())
-          .petRestriction(f.petRestriction())
-          .tel(f.tel())
-          .url(f.url())
-          .postalCode(f.postalCode())
-          .address(f.address())
-          .latitude(f.latitude())
-          .longitude(f.longitude())
-          .rawDescription(f.rawDescription())
-          .build();
+      Place e = new Place(
+          null,                         // id
+          f.uniqueKey(),                // uniqueKey
+          f.name(),                     // name
+          f.category1(),                // category1
+          f.category2(),                // category2
+          f.openingHours(),             // openingHours
+          f.closedDays(),               // closedDays
+          f.parking(),                  // parking
+          f.petAllowed(),               // petAllowed
+          f.petRestriction(),           // petRestriction
+          f.tel(),                      // tel
+          f.url(),                      // url
+          f.postalCode(),               // postalCode
+          f.address(),                  // address
+          f.latitude(),                 // latitude
+          f.longitude(),                // longitude
+          f.rawDescription(),           // rawDescription
+          0.0,                          // averageRating
+          0                             // totalReviewCount
+      );
       placeRepository.save(e);
     }
   }
