66a78ff2054305c536a0bf915e4e18e967ea05c3
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-11-17 12:25:06 +0900

    feat: 필드에 ?추가(null일 때, 해당 메시지로 예외처리를 하기 위해서)

--- backend/src/main/java/com/backend/petplace/domain/user/dto/request/UserSignupRequest.kt
+++ backend/src/main/java/com/backend/petplace/domain/user/dto/request/UserSignupRequest.kt
@@ -10,7 +10,7 @@ data class UserSignupRequest(
     @field:NotBlank(message = "이름은 필수입니다.")
     @field:Size(min = 2, max = 12, message = "이름은 2 ~ 12자까지 가능합니다.")
     @field:Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "영문, 숫자, 한글만 사용할 수 있습니다.")
-    val nickName: String,
+    val nickName: String?,
 
     @field:NotBlank(message = "비밀번호는 필수입니다.")
     @field:Size(min = 8, max = 12, message = "비밀번호는 8 ~ 12자까지 가능합니다.")
@@ -18,20 +18,20 @@ data class UserSignupRequest(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}\\[\\]:;<>,.?~\\-=/])",
         message = "영어, 숫자, 특수문자를 모두 포함해야 합니다."
     )
-    val password: String,
+    val password: String?,
 
     @field:NotBlank(message = "이메일은 필수입니다.")
     @field:Email
-    val email: String,
+    val email: String?,
 
     @field:NotBlank(message = "인증번호는 필수입니다.")
-    val authCode: String,
+    val authCode: String?,
 
     @field:NotBlank(message = "주소는 필수입니다.")
-    val address: String,
+    val address: String?,
 
     @field:NotBlank(message = "우편번호는 필수입니다.")
-    val zipcode: String,
+    val zipcode: String?,
 
     val addressDetail: String?
 )
